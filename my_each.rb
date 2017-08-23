e3fb0ca358d198a892095c8dedab446b6254c12c
Elbin Keeppanasseril <elbincodes@gmail.com>
2017-08-23 03:15:25 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
+def my_each(arr) # put argument(s) here
   # code here
+  if block_given?
+  counter = 0
+  while counter < arr.length
+    yield (arr[counter])
+    counter += 1
+  end
+    arr
+  else
+  end
 end
